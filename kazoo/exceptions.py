14afabf97358b2dc174d163d4cfe70f5f4fada6f
David LaBissoniere <labisso@gmail.com>
2012-02-27 17:27:02 -0600

    Expose the ZK exception types in kazoo.exceptions

---
+++ kazoo/exceptions.py
@@ -0,0 +1,8 @@
+from zookeeper import SystemErrorException, RuntimeInconsistencyException,\
+    DataInconsistencyException, ConnectionLossException,\
+    MarshallingErrorException,UnimplementedException,OperationTimeoutException,\
+    BadArgumentsException,ApiErrorException, NoNodeException, NoAuthException,\
+    BadVersionException,NoChildrenForEphemeralsException,NodeExistsException,\
+    InvalidACLException, AuthFailedException, NotEmptyException,\
+    SessionExpiredException, InvalidCallbackException
+  
